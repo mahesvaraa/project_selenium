7b09fdb342a7f5bbd2ec38c8c3ccf91d9b851252
Danila <Danilashkirdow@mail.ru>
2022-02-22 19:55:31 +0400

    add fixture setup

--- pages/locators.py
+++ pages/locators.py
@@ -30,4 +30,5 @@ class BasketPageLocators:
 class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
     OPEN_BASKET = (By.CSS_SELECTOR, 'div.basket-mini.pull-right.hidden-xs > span > a')
--- test_product_page.py
+++ test_product_page.py
@@ -7,95 +7,60 @@ from .pages.basket_page import BasketPage
 from .pages.login_page import LoginPage
 from .pages.product_page import ProductPage
 
-"""
-ДИСКЛЕЙМЕР!
-следующие тесты по заданию у меня вызываются с названием user, но от двух классов
-test_user_can_add_product_to_basket
-test_guest_can_add_product_to_basket
-test_guest_cant_see_product_in_basket_opened_from_product_page
-test_guest_can_go_to_login_page_from_product_page
-
-т.е в выводе они будут соответственно выглядят так:                                                                             
-test_product_page.py::TestUser::test_user_can_add_product_to_basket
-test_product_page.py::TestGuest::test_user_can_add_product_to_basket
-test_product_page.py::TestGuest::test_user_cant_see_product_in_basket_opened_from_product_page
-test_product_page.py::TestGuest::test_user_can_go_to_login_page_from_product_page  
-
-я делал от наследования - функции реализованы один раз, но вызываются дважды, с авторизацией (как user) и без (guest)
-НЕ СНИЖАЙ БАЛЛЫ ЗА ТО, ЧТО НЕ ПО ЗАДАНИЮ НАЗВАНЫ, ПОЖАЛУЙСТА <3
-"""
-
-
-def registration_user(page, need_auth=False):
-    if need_auth:
-        page.open()
-        page.go_to_login_page()
-        page = LoginPage(page.browser, page.browser.current_url)
-        page.register()
-
-
-def login_user(page, need_auth=False):
-    if need_auth:
-        page.open()
-        page.go_to_login_page()
-        page = LoginPage(page.browser, page.browser.current_url)
-        page.login()
-
 
 class TestUser:
-    need_auth = True
 
-    def test_user_cant_see_success_message_with_registration(self, browser):
-        if not self.need_auth:
-            pytest.skip('register not need for guest')
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
         url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207?promo=offer9"
-        page = BasePage(browser, url)
-        registration_user(page, self.need_auth)
+        page = ProductPage(browser, url)
         page.open()
-        sleep(3)
-
-    def test_user_cant_see_success_message_with_login(self, browser):
-        url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207?promo=offer9"
-        page = BasePage(browser, url)
-        login_user(page, self.need_auth)
-        page.open()
-        sleep(3)
-
-    @pytest.mark.need_review
-    def test_user_cant_see_product_in_basket_opened_from_product_page(self, browser):
-        if self.need_auth:
-            pytest.skip('basket is not empty with auth')
-        url = 'https://selenium1py.pythonanywhere.com/catalogue/the-girl-who-played-with-non-fire_203/'
-        page = BasePage(browser, url)
-        login_user(page, self.need_auth)
-        page.open()
-        page.go_to_the_basket()
-        page = BasketPage(browser, browser.current_url)
-        page.check_empty_basket()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.register()
+        login_page.should_be_authorized_user()
 
     @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         url = 'https://selenium1py.pythonanywhere.com/catalogue/the-girl-who-played-with-non-fire_203/'
         page = ProductPage(browser, url)
-        login_user(page, self.need_auth)
-        sleep(1)
         page.open()
         page.get_product_name()
         page.add_to_basket_button()
         page.go_to_the_basket()
         page = BasketPage(browser, url)
         page.product_is_exist_in_basket()
-        sleep(3)
+        sleep(2)
+
+
+class TestGuest:
 
     @pytest.mark.need_review
-    def test_user_can_go_to_login_page_from_product_page(self, browser):
-        if self.need_auth:
-            pytest.skip('test as user no need fo review')
+    def test_guest_cant_see_product_in_basket_opened_from_product_page(self, browser):
         url = 'https://selenium1py.pythonanywhere.com/catalogue/the-girl-who-played-with-non-fire_203/'
         page = BasePage(browser, url)
-        login_user(page, self.need_auth)
-        sleep(1)
+        page.open()
+        page.go_to_the_basket()
+        page = BasketPage(browser, browser.current_url)
+        page.check_empty_basket()
+        sleep(2)
 
+    @pytest.mark.need_review
+    def test_guest_can_add_product_to_basket(self, browser):
+        url = 'https://selenium1py.pythonanywhere.com/catalogue/the-girl-who-played-with-non-fire_203/'
+        page = ProductPage(browser, url)
+        page.open()
+        page.get_product_name()
+        page.add_to_basket_button()
+        page.go_to_the_basket()
+        page = BasketPage(browser, url)
+        page.product_is_exist_in_basket()
+        sleep(2)
 
-class TestGuest(TestUser):
-    need_auth = False
+    @pytest.mark.need_review
+    def test_guest_can_go_to_login_page_from_product_page(self, browser):
+        url = 'https://selenium1py.pythonanywhere.com/catalogue/the-girl-who-played-with-non-fire_203/'
+        page = ProductPage(browser, url)
+        page.open()
+        page.go_to_login_page()
+        sleep(2)
